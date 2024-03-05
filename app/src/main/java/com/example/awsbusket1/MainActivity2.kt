04664c73f20d10e2c57ff78b39b5da9877e31a37
mounika <mounika@wgsindia.com>
2024-03-05 10:22:37 +0530

    toastadded

--- app/src/main/java/com/example/awsbusket1/MainActivity2.kt
+++ app/src/main/java/com/example/awsbusket1/MainActivity2.kt
@@ -2,10 +2,12 @@ package com.example.awsbusket1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        Toast.makeText(this@MainActivity2,"hi",Toast.LENGTH_LONG).show()
     }
 }
